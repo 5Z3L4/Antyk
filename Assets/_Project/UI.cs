9c030ba2120f0937b62cc56c56eeb88b7d49effa
5Z3L4 <58202728+5Z3L4@users.noreply.github.com>
2024-11-18 23:24:10 +0100

    utils + ui

--- Assets/_Project/UI.cs
+++ Assets/_Project/UI.cs
@@ -1,13 +1,11 @@
+using TMPro;
 using UnityEngine;
-using UnityEngine.UI;
 
 public class UI : MonoBehaviour
 {
-    public Text text;
-
     void Update()
     {
-        var tests = FindObjectsOfType<Text>();
+        var tests = FindObjectsOfType<TMP_Text>();
         foreach (var text in tests)
         {
             if (double.TryParse(text.text, out _))
@@ -15,12 +13,5 @@ public class UI : MonoBehaviour
                 text.text = Time.time.ToString();
             }
         }
-    }
-
-    void Stop() {
-#if UNITY_EDITOR
-        UnityEditor.EditorApplication.isPlaying = false;
-#endif
-        Debug.Log("Level complete, stopping playmode\n");
     }
 }
